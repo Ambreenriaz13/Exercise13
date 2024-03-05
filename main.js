317f44a8c8476c9f13c5979a954379ef8c845c08
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-03-06 01:00:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var transpotationModes = ["sport Bike", "Bus", "Car", "Taxxi"];
+transpotationModes.forEach(function (mode) { return console.log("I would like to own a ".concat(mode)); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+let transpotationModes = ["sport Bike" ,"Bus" ,"Car" ,"Taxxi"];
+
+transpotationModes.forEach(mode => console.log(`I would like to own a ${mode}`));
